82ef5cffb4e7e1e1e99f912dcd9edb53b80b0c1f
wcrd <wcrd@pop-os.localdomain>
2023-04-20 20:51:00 -0600

    - Get basic grid up and running - Get basic fake data ready - Get Tree View operational

---
+++ PLAN.md
@@ -0,0 +1,14 @@
+# Interactive Tree Grid Builder
+
+## Needs
+
+* Go from a flat list to a nested one.
+* Two types of rows
+    * Entities
+    * Points
+* Points must be nested under an entity
+* Pressing TAB will nest
+    * Point under next entity above
+    * Entity under next entity above
+* Pressing {X} + TAB will create a new entity to nest under
+* Drag and drop to nest as well
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import AgGridSvelte from "ag-grid-svelte";
+    import 'ag-grid-enterprise'
+    import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+    import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+    const columnDefs = [
+        { field: "subject_path" }, 
+        { field: "subject" }, 
+        { field: "label" },
+        { field: "class" }
+    ];
+
+    let rowData = [];
+    function onGridReady() {
+        fetch("/fake-data.json")
+            .then((resp) => resp.json())
+            .then((data) => (rowData = data));
+    }
+
+    let gridOptions = {
+        treeData: true,
+        getDataPath: (data) => {
+            return data.subject_path ? [...data.subject_path.split("/"), data.subject] : [data.subject]
+        },
+        defaultColDef: {
+            sortable: true
+        },
+
+    };
+</script>
+
+<div class="ag-theme-alpine h-screen w-full flex flex-col">
+    <AgGridSvelte {rowData} {columnDefs} {onGridReady} {gridOptions} class=""/>
+</div>
