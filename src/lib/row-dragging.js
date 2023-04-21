2405079816b42a592ac09ac0235ba47a578f67e9
wcrd <wcrd@pop-os.localdomain>
2023-04-20 23:10:18 -0600

    - Refactor row dragging code into seperate library

--- src/lib/row-dragging.js
+++ src/lib/row-dragging.js
@@ -1,195 +1,171 @@
-var valueFormatter = function (params) {
-    return params.value ? params.value + ' MB' : '';
-  };
-  
-  var cellClassRules = {
-    'hover-over': (params) => {
-      return params.node === potentialParent;
-    },
-  };
-  
-  const gridOptions = {
-    columnDefs: [
-      {
-        field: 'dateModified',
-        cellClassRules: cellClassRules,
-      },
-      {
-        field: 'size',
-        valueFormatter: valueFormatter,
-        cellClassRules: cellClassRules,
-      },
-    ],
-    defaultColDef: {
-      flex: 1,
-      resizable: true,
-    },
-    rowData: getData(),
-    treeData: true,
-    animateRows: true,
-    groupDefaultExpanded: -1,
-    getDataPath: (data) => {
-      return data.filePath;
-    },
-    getRowId: (params) => {
-      return params.data.id;
-    },
-    autoGroupColumnDef: {
-      rowDrag: true,
-      headerName: 'Files',
-      minWidth: 300,
-      cellRendererParams: {
-        suppressCount: true,
-        innerRenderer: FileCellRenderer,
-      },
-      cellClassRules: {
-        'hover-over': (params) => {
-          return params.node === potentialParent;
-        },
-      },
-    },
-    onRowDragEnd: onRowDragEnd,
-    onRowDragMove: onRowDragMove,
-    onRowDragLeave: onRowDragLeave,
-  };
-  
-  var potentialParent = null;
-  
-  function onRowDragMove(event) {
-    setPotentialParentForNode(event.api, event.overNode);
-  }
-  
-  function onRowDragLeave(event) {
-    // clear node to highlight
-    setPotentialParentForNode(event.api, null);
-  }
-  
-  function onRowDragEnd(event) {
-    if (!potentialParent) {
-      return;
-    }
-  
-    var movingData = event.node.data;
-  
-    // take new parent path from parent, if data is missing, means it's the root node,
-    // which has no data.
-    var newParentPath = potentialParent.data ? potentialParent.data.filePath : [];
-    var needToChangeParent = !arePathsEqual(newParentPath, movingData.filePath);
-  
-    // check we are not moving a folder into a child folder
-    var invalidMode = isSelectionParentOfTarget(event.node, potentialParent);
-    if (invalidMode) {
-      console.log('invalid move');
-    }
-  
-    if (needToChangeParent && !invalidMode) {
-      var updatedRows = [];
-      moveToPath(newParentPath, event.node, updatedRows);
-  
-      gridOptions.api.applyTransaction({
-        update: updatedRows,
-      });
-      gridOptions.api.clearFocusedCell();
-    }
-  
-    // clear node to highlight
-    setPotentialParentForNode(event.api, null);
-  }
-  
-  function moveToPath(newParentPath, node, allUpdatedNodes) {
-    // last part of the file path is the file name
-    var oldPath = node.data.filePath;
-    var fileName = oldPath[oldPath.length - 1];
-    var newChildPath = newParentPath.slice();
-    newChildPath.push(fileName);
-  
-    node.data.filePath = newChildPath;
-  
-    allUpdatedNodes.push(node.data);
-  
-    if (node.childrenAfterGroup) {
-      node.childrenAfterGroup.forEach(function (childNode) {
-        moveToPath(newChildPath, childNode, allUpdatedNodes);
-      });
-    }
-  }
-  
-  function isSelectionParentOfTarget(selectedNode, targetNode) {
-    var children = selectedNode.childrenAfterGroup || [];
-    for (var i = 0; i < children.length; i++) {
-      if (targetNode && children[i].key === targetNode.key) return true;
-      isSelectionParentOfTarget(children[i], targetNode);
-    }
-    return false;
-  }
-  
-  function arePathsEqual(path1, path2) {
-    if (path1.length !== path2.length) {
-      return false;
-    }
-  
-    var equal = true;
-    path1.forEach(function (item, index) {
-      if (path2[index] !== item) {
-        equal = false;
-      }
-    });
-  
-    return equal;
-  }
-  
-  function setPotentialParentForNode(api, overNode) {
-    var newPotentialParent;
+// ###################################################
+// Row moving controllers
+//
+
+let potentialParent = null;
+
+function onRowDragMove(event){
+    setPotentialParentForNode(event.api, event.overNode)
+}
+
+
+// Single Selection Only
+
+function setPotentialParentForNode(gridApi, overNode){
+    let newPotentialParent;
+    
+    // set parent to node we are hovering near
     if (overNode) {
-      newPotentialParent =
-        overNode.data.type === 'folder'
-          ? // if over a folder, we take the immediate row
+    newPotentialParent =
+        overNode.data?.type === 'entity'
+        ? // if over an entity, we take the immediate row
             overNode
-          : // if over a file, we take the parent row (which will be a folder)
+        : // if over a point, we take the parent row (which will be an entity, or root)
             overNode.parent;
     } else {
-      newPotentialParent = null;
+        newPotentialParent = null;
     }
-  
-    var alreadySelected = potentialParent === newPotentialParent;
+
+    // check if it is the current parent
+    const alreadySelected = potentialParent === newPotentialParent;
     if (alreadySelected) {
-      return;
+        return; // exit early, nothing to change
     }
-  
+
+    // if a new parent, lets remove highlight from old potential parent and highligh this one
     // we refresh the previous selection (if it exists) to clear
     // the highlighted and then the new selection.
-    var rowsToRefresh = [];
+    const rowsToRefresh = [];
     if (potentialParent) {
-      rowsToRefresh.push(potentialParent);
+        rowsToRefresh.push(potentialParent);
     }
     if (newPotentialParent) {
-      rowsToRefresh.push(newPotentialParent);
+        rowsToRefresh.push(newPotentialParent);
     }
-  
+
     potentialParent = newPotentialParent;
-  
-    refreshRows(api, rowsToRefresh);
-  }
-  
-  function refreshRows(api, rowsToRefresh) {
+
+    refreshRows(gridApi, rowsToRefresh);
+};
+
+function refreshRows(api, rowsToRefresh) {
     var params = {
-      // refresh these rows only.
-      rowNodes: rowsToRefresh,
-      // because the grid does change detection, the refresh
-      // will not happen because the underlying value has not
-      // changed. to get around this, we force the refresh,
-      // which skips change detection.
-      force: true,
+    // refresh these rows only.
+    rowNodes: rowsToRefresh,
+    // because the grid does change detection, the refresh
+    // will not happen because the underlying value has not
+    // changed. to get around this, we force the refresh,
+    // which skips change detection.
+    force: true,
     };
     api.refreshCells(params);
-  }
-  
-  // wait for the document to be loaded, otherwise
-  // AG Grid will not find the div in the document.
-  document.addEventListener('DOMContentLoaded', function () {
-    // lookup the container we want the Grid to use
-    var eGridDiv = document.querySelector('#myGrid');
-  
-    // create the grid passing in the div to use together with the columns & data we want to use
-    new agGrid.Grid(eGridDiv, gridOptions);
-  });
+}
+
+function onRowDragEnd(event){
+    if(!potentialParent){
+        return
+    }
+
+    // Updated to support multiple selection dragging
+    const newParentPath = potentialParent.data ? potentialParent.data.subject_path : [];
+    // filter to just nodes that need parents updated
+    const nodesToChangeParent = event.nodes.filter((node) => {
+        if(!arePathsEqual(newParentPath, node.data.subject_path)){
+            return true
+        };
+        return false
+    });
+
+    // check if any nodes that are being updated are being moved into their own child
+    const invalidMode = nodesToChangeParent.some(node => isSelectionParentOfTarget(node, potentialParent));
+    if (invalidMode) {
+        console.log('Not allowed.');
+    }
+
+    if (nodesToChangeParent.length>0 && !invalidMode) {
+        const updatedRows = [];
+        // for each node, lets move path
+        nodesToChangeParent.forEach(node => {
+            moveToPath(newParentPath, node, updatedRows);
+        });
+        event.api.applyTransaction({
+            update: updatedRows,
+        });
+        event.api.clearFocusedCell();
+    }
+    
+    // OLD - Single selection dragging ONLY
+    // const rowDataToMove = event.node.data;
+    // // console.debug("Row data to move: ", rowDataToMove, "Potential Parent: ", potentialParent)
+    // // take new parent path from parent, if data is missing, means it's the root node,
+    // // which has no data.
+    // const newParentPath = potentialParent.data ? potentialParent.data.subject_path : [];
+    // const needToChangeParent = !arePathsEqual(newParentPath, rowDataToMove.subject_path);
+
+    // // check we are not moving a entity into a child entity
+    // const invalidMode = isSelectionParentOfTarget(event.node, potentialParent);
+    // if (invalidMode) {
+    //     console.log('Not allowed.');
+    // }
+
+    // if (needToChangeParent && !invalidMode) {
+    //     const updatedRows = [];
+    //     moveToPath(newParentPath, event.node, updatedRows);
+    
+    //     event.api.applyTransaction({
+    //         update: updatedRows,
+    //     });
+    //     event.api.clearFocusedCell();
+    // }
+
+
+    // clear node to highlight
+    setPotentialParentForNode(event.api, null);
+
+
+}
+
+function arePathsEqual(path1, path2) {
+    if (path1.length !== path2.length) {
+        return false;
+    }
+
+    let equal = true;
+    path1.forEach((item, index) => {
+        if (path2[index] !== item) {
+            equal = false;
+        }
+    });
+
+    return equal;
+}
+
+function isSelectionParentOfTarget(selectedNode, targetNode) {
+    const children = selectedNode.childrenAfterGroup || [];
+    for (let i = 0; i < children.length; i++) {
+        if (targetNode && children[i].key === targetNode.key) return true;
+            isSelectionParentOfTarget(children[i], targetNode);
+        }
+    return false;
+}
+
+function moveToPath(newParentPath, node, allUpdatedNodes) {
+    // last part of the file path is the file name
+    const oldPath = node.data.subject_path;
+    const leafName = oldPath.pop();
+    const newChildPath = newParentPath.slice();
+    newChildPath.push(leafName);
+
+    node.data.subject_path = newChildPath;
+
+    allUpdatedNodes.push(node.data);
+    
+    // if the node we move has children of its own, they need to be updated.
+    if (node.childrenAfterGroup) {
+        node.childrenAfterGroup.forEach((childNode) => {
+            moveToPath(newChildPath, childNode, allUpdatedNodes);
+        });
+    }
+}
+
+export { potentialParent, onRowDragEnd, onRowDragMove }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
     import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
     import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
+    import { potentialParent, onRowDragEnd, onRowDragMove } from '$lib/row-dragging.js'
+
     const cellClassRules = {
         'hover-over': (params) => {return params.node === potentialParent}
     };
@@ -43,9 +45,9 @@
             rowDrag: true,
         },
         // Row Dragging Config (Event Handlers for native Grid Events)
-        onRowDragEnter: e => {
-            console.debug("Row Drag Begin: ", e)
-        },
+        // onRowDragEnter: e => {
+        //     console.debug("Row Drag Begin: ", e)
+        // },
         onRowDragLeave: e => {
             console.debug("You left the grid yo stupid fok")
         },
@@ -53,177 +55,6 @@
         onRowDragEnd: onRowDragEnd
 
     };
-    
-    // ###################################################
-    // Row moving controllers
-    //
-
-    let potentialParent = null;
-
-    function onRowDragMove(event){
-        setPotentialParentForNode(event.api, event.overNode)
-    }
-
-
-    // Single Selection Only
-
-    function setPotentialParentForNode(gridApi, overNode){
-        let newPotentialParent;
-        
-        // set parent to node we are hovering near
-        if (overNode) {
-        newPotentialParent =
-            overNode.data?.type === 'entity'
-            ? // if over an entity, we take the immediate row
-                overNode
-            : // if over a point, we take the parent row (which will be an entity, or root)
-                overNode.parent;
-        } else {
-            newPotentialParent = null;
-        }
-
-        // check if it is the current parent
-        const alreadySelected = potentialParent === newPotentialParent;
-        if (alreadySelected) {
-            return; // exit early, nothing to change
-        }
-
-        // if a new parent, lets remove highlight from old potential parent and highligh this one
-        // we refresh the previous selection (if it exists) to clear
-        // the highlighted and then the new selection.
-        const rowsToRefresh = [];
-        if (potentialParent) {
-            rowsToRefresh.push(potentialParent);
-        }
-        if (newPotentialParent) {
-            rowsToRefresh.push(newPotentialParent);
-        }
-    
-        potentialParent = newPotentialParent;
-    
-        refreshRows(gridApi, rowsToRefresh);
-    };
-
-    function refreshRows(api, rowsToRefresh) {
-        var params = {
-        // refresh these rows only.
-        rowNodes: rowsToRefresh,
-        // because the grid does change detection, the refresh
-        // will not happen because the underlying value has not
-        // changed. to get around this, we force the refresh,
-        // which skips change detection.
-        force: true,
-        };
-        api.refreshCells(params);
-    }
-
-    function onRowDragEnd(event){
-        if(!potentialParent){
-            return
-        }
-
-        // Updated to support multiple selection dragging
-        const newParentPath = potentialParent.data ? potentialParent.data.subject_path : [];
-        // filter to just nodes that need parents updated
-        const nodesToChangeParent = event.nodes.filter((node) => {
-            if(!arePathsEqual(newParentPath, node.data.subject_path)){
-                return true
-            };
-            return false
-        });
-
-        // check if any nodes that are being updated are being moved into their own child
-        const invalidMode = nodesToChangeParent.some(node => isSelectionParentOfTarget(node, potentialParent));
-        if (invalidMode) {
-            console.log('Not allowed.');
-        }
-
-        if (nodesToChangeParent.length>0 && !invalidMode) {
-            const updatedRows = [];
-            // for each node, lets move path
-            nodesToChangeParent.forEach(node => {
-                moveToPath(newParentPath, node, updatedRows);
-            });
-            event.api.applyTransaction({
-                update: updatedRows,
-            });
-            event.api.clearFocusedCell();
-        }
-        
-        // OLD - Single selection dragging ONLY
-        // const rowDataToMove = event.node.data;
-        // // console.debug("Row data to move: ", rowDataToMove, "Potential Parent: ", potentialParent)
-        // // take new parent path from parent, if data is missing, means it's the root node,
-        // // which has no data.
-        // const newParentPath = potentialParent.data ? potentialParent.data.subject_path : [];
-        // const needToChangeParent = !arePathsEqual(newParentPath, rowDataToMove.subject_path);
-    
-        // // check we are not moving a entity into a child entity
-        // const invalidMode = isSelectionParentOfTarget(event.node, potentialParent);
-        // if (invalidMode) {
-        //     console.log('Not allowed.');
-        // }
-  
-        // if (needToChangeParent && !invalidMode) {
-        //     const updatedRows = [];
-        //     moveToPath(newParentPath, event.node, updatedRows);
-        
-        //     event.api.applyTransaction({
-        //         update: updatedRows,
-        //     });
-        //     event.api.clearFocusedCell();
-        // }
-
-
-        // clear node to highlight
-        setPotentialParentForNode(event.api, null);
-
-    
-    }
-
-    function arePathsEqual(path1, path2) {
-        if (path1.length !== path2.length) {
-            return false;
-        }
-    
-        let equal = true;
-        path1.forEach((item, index) => {
-            if (path2[index] !== item) {
-                equal = false;
-            }
-        });
-    
-        return equal;
-    }
-
-    function isSelectionParentOfTarget(selectedNode, targetNode) {
-        const children = selectedNode.childrenAfterGroup || [];
-        for (let i = 0; i < children.length; i++) {
-            if (targetNode && children[i].key === targetNode.key) return true;
-                isSelectionParentOfTarget(children[i], targetNode);
-            }
-        return false;
-    }
-
-    function moveToPath(newParentPath, node, allUpdatedNodes) {
-        // last part of the file path is the file name
-        const oldPath = node.data.subject_path;
-        const leafName = oldPath.pop();
-        const newChildPath = newParentPath.slice();
-        newChildPath.push(leafName);
-    
-        node.data.subject_path = newChildPath;
-    
-        allUpdatedNodes.push(node.data);
-        
-        // if the node we move has children of its own, they need to be updated.
-        if (node.childrenAfterGroup) {
-            node.childrenAfterGroup.forEach((childNode) => {
-                moveToPath(newChildPath, childNode, allUpdatedNodes);
-            });
-        }
-    }
-
     
 </script>
 
