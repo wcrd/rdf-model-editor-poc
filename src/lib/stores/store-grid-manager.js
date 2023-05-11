3114ca7cd677a0f69139e3c4bd3280e798c7b0d0
wcrd <wcrd@pop-os.localdomain>
2023-05-11 10:29:52 -0600

    - Update old store to remove model items

--- src/lib/stores/store-grid-manager.js
+++ src/lib/stores/store-grid-manager.js
@@ -1,18 +1,18 @@
 import { writable, get } from "svelte/store";
 
 // Grid API references
-export const modelGridAPI = writable();
+// export const modelGridAPI = writable();
 export const sourceGridAPI = writable();
 
 // Grid column defs; these can also be had via getColumnDefs but it may be nicer to keep them as reactive var.
-export const modelGridColumnDefs = writable();
+// export const modelGridColumnDefs = writable();
 export const sourceGridColumnDefs = writable();
 
 // Grid Data references
-export const modelData = writable([]);
+// export const modelData = writable([]);
 export const sourceData = writable([]);
 
 // change tracking
 export const sourceEditedNodes = writable()
 
-console.debug({get, modelData, sourceData, modelGridAPI, sourceGridAPI, sourceEditedNodes})
+console.debug({get, sourceData, sourceGridAPI, sourceEditedNodes})
